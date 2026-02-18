109c6ea13d32876ffe03f8e6a7e353b0b1f61e15
Asif <asifsaascraft@gmail.com>
2026-02-18 12:42:14 +0530

    populate branch name & code in user login response

--- controllers/userController.js
+++ controllers/userController.js
@@ -264,7 +264,9 @@ export const loginUser = async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    const user = await User.findOne({ email });
+    const user = await User.findOne({ email })
+      .populate("branchId", "branchName branchCode");
+
     if (!user) {
       return res.status(400).json({ message: "Email does not exist" });
     }
@@ -290,13 +292,14 @@ export const loginUser = async (req, res) => {
         id: user._id,
         name: user.name,
         email: user.email,
-        branchId: user.branchId,
+        branch: user.branchId, // ✅ populated branch object
       },
     });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
+
 
 /* ======================================================
    Refresh Access Token
